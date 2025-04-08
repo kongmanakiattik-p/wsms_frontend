b8cd00fab3f99b25d5b12a253cd57911c54e75a4
Akarawit01 <juntarang_a@silpakorn.edu>
2025-04-08 13:51:59 +0700

    frontEnd-1

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <template>
-  <div class="app-container">
-    <NavBar />
-    <main class="main-content">
-      <CarDetail />
-      
-      <div class="services-section">
-        <ServiceCard v-for="(service, index) in serviceList" :key="index" :service="service" />
-      </div>
-    </main>
-  </div>
+    <LoginUser />
+    <LoginAffiliator />
+    <AffiliatorForm />
+    <UserForm />
+    <SearchCar />
 </template>
 
 <script setup>
-import NavBar from './components/NavBar.vue';
-import CarDetail from './pages/carDetail.vue';
-import ServiceCard from './components/ServiceCard.vue';
-
-const serviceList = [
-  {
-    name: 'Service Name',
-    description: 'This is the space to describe the service. Focus the description on how customers or clients can benefit from using this service: explain how it solves a problem, or makes life easier or more enjoyable.',
-    details: 'Be sure to include all the relevant details users will want to know, like pricing, duration, and location. If they\'ll need to prepare or bring anything with them, let them know here. Give users an idea of what to expect from the service and tell them how to book it.'
-  },
-  {
-    name: 'Service Name',
-    description: 'This is the space to describe the service. Focus the description on how customers or clients can benefit from using this service: explain how it solves a problem, or makes life easier or more enjoyable.',
-    details: 'Be sure to include all the relevant details users will want to know, like pricing, duration, and location. If they\'ll need to prepare or bring anything with them, let them know here. Give users an idea of what to expect from the service and tell them how to book it.'
-  },
-  {
-    name: 'Service Name',
-    description: 'This is the space to describe the service. Focus the description on how customers or clients can benefit from using this service: explain how it solves a problem, or makes life easier or more enjoyable.',
-    details: 'Be sure to include all the relevant details users will want to know, like pricing, duration, and location. If they\'ll need to prepare or bring anything with them, let them know here. Give users an idea of what to expect from the service and tell them how to book it.'
-  }
-];
+  import LoginUser from './pages/LoginUser.vue' ;
+  import LoginAffiliator from './pages/LoginAffiliator.vue';
+  import AffiliatorForm from './pages/AffiliatorForm.vue';
+  import UserForm from './pages/UserForm.vue';
+  import SearchCar from './pages/SearchCar.vue';
 </script>
 
 <style scoped>
